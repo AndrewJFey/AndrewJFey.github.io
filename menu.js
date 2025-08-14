e5e2839817021d6096094b062a6bb88d91b99223
AndrewJFey <157545125+AndrewJFey@users.noreply.github.com>
2025-08-14 15:05:17 -0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -5,9 +5,11 @@ const overlay = document.querySelector('.overlay');
 menuButton.addEventListener('click', () => {
     leftPanel.classList.toggle('active');
     overlay.classList.toggle('active');
+    document.body.classList.toggle('menu-open'); // lock/unlock body scroll
 });
 
 overlay.addEventListener('click', () => {
     leftPanel.classList.remove('active');
     overlay.classList.remove('active');
+    document.body.classList.remove('menu-open');
 });
